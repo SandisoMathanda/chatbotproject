8e6e311e93d23d48e165cde583767348faed3da2
Sandiso Mathanda <sandisomathanda@gmail.com>
2016-09-27 14:03:58 -0700

    undefined
    made a response for undefined

--- chatbot.js
+++ chatbot.js
@@ -3,6 +3,7 @@ var botleyString = "Botley: ";
 var meResult = meString.bold();
 var botleyResult = botleyString.bold();
 var now = new Date(Date.now());
+//var now = now.get
 var hh = now.getHours();
 var hours = hh;
 var meridian = "AM";
@@ -24,11 +25,18 @@ var response = {
   "What is your name?": "Botley. Isn't it obvious!?",
   "What is your real name?": "The name's Bond. James Bond.",
   "Tell me your life story.": "It all started one day in a small village in Yugoslavia... ",
-  "What time is it?": "It is " +  hours + ":" + minutes + " " + meridian
+  "What time is it?": "It is " +  hours + ":" + minutes + " " + meridian,
+  "Whither goest thou, young rogue?": "Over to the next town fine sir"
 }
 function displayInput() {
   var question = $("#input").val();
 
-$("#chat-area").prepend( meResult + question + "<br>"
+if (response[question] == undefined) {
+  $("#chat-area").prepend( meResult + question + "<br>"
+  + botleyResult + "Sorry answer not found" + "<hr>");
+}
+
+else {$("#chat-area").prepend( meResult + question + "<br>"
 + botleyResult + response[question] + "<hr>");
+  }
 }
